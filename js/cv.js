7a5b10c1d2082496cd174d0c4bb3f495cc331416
khakimonk <khakimonk@gmail.com>
2016-11-11 15:01:16 +0000

    Updated styling

--- js/cv.js
+++ js/cv.js
@@ -1,26 +1,7 @@
-var popMe = document.getElementById('popMe');
-var aboutMe = document.getElementById('aboutMe');
-var popCareer = document.getElementById('popCareer');
-var myCareer = document.getElementById('myCareer');
-var popProjects = document.getElementById('popProjects');
-var myProjects = document.getElementById('myProjects');
-var btnAbout = document.getElementById('btnAbout');
-var btnCareer = document.getElementById('btnCareer');
-var btnProjects = document.getElementById('btnProjects');
-var btnLinks = document.getElementById('btnLinks');
-var body = document.getElementsByTagName('body');
-
-var aniAboutMe = btnAbout.addEventListener('click', function() {
-            var id = setInterval(frame, 5);
-
-            function frame() {
-                var height = 0;
-                if (height == 50) {
-                    clearInterval(id);
-                } else {
-                    height++;
-                    popMe.style.height = 50 + '%';
-                    aboutMe.style.display = 'block';
-                }
-              }
-            });
+$(function() {
+    $("#accordion").accordion({
+    collapsible: true,
+    heightStyle: "content",
+    active: false
+    });
+});
